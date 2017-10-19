d2012d35fdc3593fa5890ffd517a8af986a6f73d
jswhit <whitaker.jeffrey@gmail.com>
2017-10-19 23:57:59 +0000

    update

--- config.sh
+++ config.sh
@@ -228,7 +228,7 @@ export saterrfact=1.0
 export deterministic=.true.
 export sortinc=.true.
                                                                     
-export nitermax=2 
+export nitermax=1 
 
 export enkfscripts="${basedir}/scripts/${exptname}"
 export homedir=$enkfscripts
@@ -265,7 +265,7 @@ elif [ "$machine" == 'wcoss' ]; then
    export FIXFV3=${fv3gfspath}/fix_fv3
    export FIXGLOBAL=${fv3gfspath}/fix/fix_am
    export fixgsi=${gsipath}/fix
-   export fixcrtm=/gpfs/hps/nco/ops/nwprod/lib/crtm/v2.2.3/fix
+   export fixcrtm=${fixgsi}/crtm_2.2.3
    export execdir=${enkfscripts}/exec_${machine}
    export enkfbin=${execdir}/global_enkf
    export FCSTEXEC=${execdir}/${fv3exec}
