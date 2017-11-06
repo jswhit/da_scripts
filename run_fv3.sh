dd8be77dbcbb7d50a85a7d85c69bafc50e17f5b8
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2017-11-06 22:06:42 +0000

    update

--- run_fv3.sh
+++ run_fv3.sh
@@ -110,7 +110,7 @@ fi
 /bin/cp -f $enkfscripts/nems.configure .
 # insert correct starting time and output interval in diag_table template.
 sed -i -e "s/YYYY MM DD HH/${year} ${mon} ${day} ${hour}/g" diag_table
-#sed -i -e "s/FHOUT/${FHOUT}/g" diag_table
+sed -i -e "s/FHOUT/${FHOUT}/g" diag_table
 if [ "$zhao_mic" == 'T' ]; then
 /bin/cp -f $enkfscripts/field_table .
 else
