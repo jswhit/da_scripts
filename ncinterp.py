148a764d7764fdb9e8fad761e828c9923d128455
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2018-03-01 11:57:36 -0500

    add metadata into latlon nc file

--- ncinterp.py
+++ ncinterp.py
@@ -9,8 +9,13 @@ nlons = 360; nlats = 181
 zlib = True; lsd = None # lossy compression, 4 significant digits
 
 datapath = sys.argv[1]
+initialdate = [d for d in datapath.split('/') if d.startswith('2')][0]
 fileout = sys.argv[2]
 res = int(sys.argv[3])
+if len(sys.argv) > 4:
+    refdate = sys.argv[4]
+else:
+    refdate = None
 
 # read in triangulation.
 picklefile = 'C%s_grid.pickle' % res
@@ -40,15 +45,24 @@ ncout = Dataset('%s/%s' % (datapath,fileout) ,'w',format='NETCDF4_CLASSIC')
 # define dimensions, coordinate vars in output file
 latd = ncout.createDimension('latitude',nlats)
 lats = ncout.createVariable('latitude',np.float32,'latitude')
+lats.units = 'degrees north'
 lats[:] =  olats_deg
 lond = ncout.createDimension('longitude',nlons)
 lons = ncout.createVariable('longitude',np.float32,'longitude')
+lons.units = 'degrees east'
 lons[:] =  olons_deg
 timed = ncout.createDimension('time',ntimes)
 t = ncout.createVariable('time',np.float32,'time')
+if refdate is None:
+    t.units = 'hours since %s-%s-%s %s:00:00' % (initialdate[0:4],initialdate[4:6],initialdate[6:8],initialdate[8:10])
+    if times[0] == 6: # replay forecasts, shift time origin
+       times = times - 6
+else:
+    t.units = 'hours since %s-%s-%s %s:00:00' % (refdate[0:4],refdate[4:6],refdate[6:8],refdate[8:10])
 t[:] = times
 levd = ncout.createDimension('plev',nlevs)
 p = ncout.createVariable('plev',np.float32,'plev')
+p.units = 'hPa'
 p[:] = plevs
 
 for varname in varnames:
@@ -58,6 +72,13 @@ for varname in varnames:
     if nc[varname].ndim == 3:
         varout = ncout.createVariable(varname, np.float32, ('time','latitude','longitude'),zlib=zlib,least_significant_digit=lsd)
         print 'processing ',varname
+        if varname.startswith('u') or varname.startswith('v'):
+            varout.units = 'm/sec'
+        if varname == 'tmp2m': varout.units = 'K'
+        if varname == 'pwat': varout.units = 'mm'
+        if varname == 'prate_ave': varout.units = 'mm/sec'
+        if varname == 'pressfc': varout.units = 'Pa'
+        if varname in ['slp','pmaskv2']: varout.units = 'hPa'
         # read cube data for this variable.
         cube_data = np.empty((ntimes,6,res,res),np.float32)
         for ntile in range(6):
@@ -75,6 +96,11 @@ for varname in varnames:
     else:
         varout = ncout.createVariable(varname, np.float32, ('time','plev','latitude','longitude'),zlib=zlib,least_significant_digit=lsd)
         print 'processing ',varname
+        if varname.startswith('u') or varname.startswith('v'):
+            varout.units = 'm/sec'
+        if varname == 'h_plev': varout.units = 'gpm'
+        if varname == 't_plev': varout.units = 'K'
+        if varname == 'q_plev': varout.units = 'kg/kg'
         # read cube data for this variable.
         cube_data = np.empty((ntimes,nlevs,6,res,res),np.float32)
         for ntile in range(6):
