bdf44822f4610d06f9be8c35f63e291121836739
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2019-12-05 22:38:56 +0000

    update

--- recenter_ens_anal.sh
+++ recenter_ens_anal.sh
@@ -12,12 +12,15 @@ charfhr="fhr"`printf %02i $nhr_anal`
 
 # control analysis is at higher resolution, change resolution and adjust topography
 if [ $controlfcst == "true" ] && [ $replay_controlfcst == 'false' ] && [ $LONB -ne $LONB_CTL ]; then
+    echo "running chgres"
     sh ${enkfscripts}/chgres.sh $datapath2/sanl_${analdate}_${charfhr}_${charnanal} $datapath2/sanl_${analdate}_${charfhr}_ensmean $datapath2/sanl_${analdate}_${charfhr}_${charnanal}.chgres 
     if [ $? -ne 0 ]; then
        echo "chgres failed, exiting.."
        echo "no" > ${current_logdir}/recenter_ens.log
        exit 1
     fi
+else
+    echo "no chgres needed since GSI was run with ens mean background"
 fi
 
 echo "recenter ensemble perturbations about low resolution hybrid analysis"
