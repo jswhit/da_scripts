ddef24505960c24a519271a01b084773c7d3d032
Jeffrey S Whitaker <jwhitake@Orion-login-3.HPC.MsState.Edu>
2020-02-22 12:45:15 -0600

    fix module loads on orion

--- run_hybridanal.sh
+++ run_hybridanal.sh
@@ -2,7 +2,12 @@
 # do hybrid analysis.
 
 export CO2DIR=$fixgsi
-
+if [ $machine == 'orion' ]; then
+   module switch intel intel/2018.4
+   module switch impi impi/2018.4
+   module switch mkl mkl/2018.4
+   module list
+fi
 export SIGANL03=${datapath2}/sanl_${analdate}_fhr03_${charnanal}
 export SIGANL04=${datapath2}/sanl_${analdate}_fhr04_${charnanal}
 export SIGANL05=${datapath2}/sanl_${analdate}_fhr05_${charnanal}
