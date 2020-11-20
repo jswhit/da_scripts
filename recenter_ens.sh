d93acf989a05dc1bdcce3a061b4deb34815cae88
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2020-11-20 22:15:02 +0000

    update

--- recenter_ens.sh
+++ recenter_ens.sh
@@ -2,7 +2,9 @@
 
 export VERBOSE=YES
 export OMP_STACKSIZE=256M
-charnanal="control"
+charnanal=${charnanal:-"control"}
+recenter_ensmean_wgt=${recenter_ensmean_wgt:-"100"}
+recenter_control_wgt=${recenter_control_wgt:-"0"}
 pushd ${datapath2}
 
 fh=${FHMIN}
@@ -10,10 +12,10 @@ while [ $fh -le $FHMAX ]; do
    charfhr="fhr"`printf %02i $fh`
    
    echo "recenter ensemble perturbations"
-   filename_meanin=sfg_${analdate}_${charfhr}_ensmean
-   filename_meanout=sfg_${analdate}_${charfhr}_${charnanal}.chgres
-   filenamein=sfg_${analdate}_${charfhr}
-   filenameout=sfgr_${analdate}_${charfhr}
+   filename_meanin=${fileprefix}_${analdate}_${charfhr}_ensmean
+   filename_meanout=${filename_meanout:-${fileprefix}_${analdate}_${charfhr}_${charnanal}}
+   filenamein=${fileprefix}_${analdate}_${charfhr}
+   filenameout=${fileprefix}r_${analdate}_${charfhr}
    
    export PGM="${execdir}/recenterens_ncio.x $filenamein $filename_meanin $filename_meanout $filenameout $nanals $recenter_ensmean_wgt $recenter_control_wgt"
    errorcode=0
@@ -36,8 +38,8 @@ while [ $fh -le $FHMAX ]; do
    nanal=1
    while [ $nanal -le $nanals ]; do
       charnanal_tmp="mem"`printf %03i $nanal`
-      analfiler=sfgr_${analdate}_${charfhr}_${charnanal_tmp}
-      analfile=sfg_${analdate}_${charfhr}_${charnanal_tmp}
+      analfiler=${fileprefix}r_${analdate}_${charfhr}_${charnanal_tmp}
+      analfile=${fileprefix}_${analdate}_${charfhr}_${charnanal_tmp}
       if [ -s $analfiler ]; then
          /bin/mv -f $analfile ${analfile}.orig
          /bin/mv -f $analfiler $analfile
@@ -62,7 +64,7 @@ while [ $fh -le $FHMAX ]; do
       nanal=1
       while [ $nanal -le $nanals ]; do
          charnanal_tmp="mem"`printf %03i $nanal`
-         analfile=sfg_${analdate}_${charfhr}_${charnanal_tmp}
+         analfile=${fileprefix}_${analdate}_${charfhr}_${charnanal_tmp}
          /bin/mv -f ${analfile}.orig ${analfile}
          nanal=$((nanal+1))
       done
