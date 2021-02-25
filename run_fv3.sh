ba2f0c07d1ab02c4ea9c37526c3af6224c1ffa74
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2021-02-25 18:10:33 +0000

    taper strat humid increment

--- run_fv3.sh
+++ run_fv3.sh
@@ -200,7 +200,7 @@ if [ "$cold_start" == "false" ] && [ -z $skip_calc_increment ]; then
       echo "create ${increment_file}"
       /bin/rm -f ${increment_file}
       # last three args:  no_mpinc no_delzinc, taper_strat
-      export "PGM=${execdir}/calc_increment_ncio.x ${fgfile} ${analfile} ${increment_file} T $hydrostatic F"
+      export "PGM=${execdir}/calc_increment_ncio.x ${fgfile} ${analfile} ${increment_file} T $hydrostatic T"
       nprocs=1 mpitaskspernode=1 ${enkfscripts}/runmpi
       if [ $? -ne 0 -o ! -s ${increment_file} ]; then
          echo "problem creating ${increment_file}, stopping .."
