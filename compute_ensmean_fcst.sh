aa8529ba1d0fa57c02081932bf7c6baba507e8ed
Jeffrey S Whitaker <jeffrey.s.whitaker@noaa.gov>
2021-04-21 20:36:48 -0500

    update

--- compute_ensmean_fcst.sh
+++ compute_ensmean_fcst.sh
@@ -19,9 +19,9 @@ while [ $fh -le 9 ] && [ -s ${datapath2}/sfg_${analdate}_${charfhr}_mem001 ]; do
   if [ $cleanup_ensmean == 'true' ] || ([ $cleanup_ensmean == 'false' ]  && [ ! -s ${datapath}/${analdate}/bfg_${analdate}_${charfhr}_ensmean ]); then
       if [ $FHMAX -eq 4 ] && [ $fh -gt 7 ] && [ $cold_start != "true" ]; then
          # symlink last two forecast times (only needed to trick GSI into believing window is symmetric)
-         /bin/ln -fs ${datapath}/${analdate}/bfg_${analdatep1}_fhr07_ensmean ${datapath}/${analdate}/bfg_${analdatep1}_${charfhr}_ensmean
+         /bin/ln -fs ${datapath}/${analdate}/bfg_${analdate}_fhr07_ensmean ${datapath}/${analdate}/bfg_${analdate}_${charfhr}_ensmean
       else
-         /bin/rm -f  ${datapath}/${analdate}/bfg_${analdatep1}_${charfhr}_ensmean
+         /bin/rm -f  ${datapath}/${analdate}/bfg_${analdate}_${charfhr}_ensmean
          echo "running  ${execdir}/getsfcensmeanp.x ${datapath2}/ bfg_${analdate}_${charfhr}_ensmean bfg_${analdate}_${charfhr} ${nanals}"
          /bin/rm -f ${datapath2}/bfg_${analdate}_${charfhr}_ensmean
          export PGM="${execdir}/getsfcensmeanp.x ${datapath2}/ bfg_${analdate}_${charfhr}_ensmean bfg_${analdate}_${charfhr} ${nanals}"
@@ -35,11 +35,11 @@ while [ $fh -le 9 ] && [ -s ${datapath2}/sfg_${analdate}_${charfhr}_mem001 ]; do
   if [ $cleanup_ensmean == 'true' ] || ([ $cleanup_ensmean == 'false' ]  && [ ! -s ${datapath}/${analdate}/sfg_${analdate}_${charfhr}_ensmean ]); then
       if [ $FHMAX -eq 4 ] && [ $fh -gt 7 ] && [ $cold_start != "true" ]; then
          # symlink last two forecast times (only needed to trick GSI into believing window is symmetric)
-         /bin/ln -fs ${datapath}/${analdate}/sfg_${analdatep1}_fhr07_ensmean ${datapath}/${analdate}/sfg_${analdatep1}_${charfhr}_ensmean
-         /bin/ln -fs ${datapath}/${analdate}/sfg_${analdatep1}_fhr07_enssprd ${datapath}/${analdate}/sfg_${analdatep1}_${charfhr}_enssprd
+         /bin/ln -fs ${datapath}/${analdate}/sfg_${analdate}_fhr07_ensmean ${datapath}/${analdate}/sfg_${analdate}_${charfhr}_ensmean
+         /bin/ln -fs ${datapath}/${analdate}/sfg_${analdate}_fhr07_enssprd ${datapath}/${analdate}/sfg_${analdate}_${charfhr}_enssprd
       else
-         /bin/rm -f  ${datapath}/${analdate}/sfg_${analdatep1}_${charfhr}_ensmean
-         /bin/rm -f  ${datapath}/${analdate}/sfg_${analdatep1}_${charfhr}_enssprd
+         /bin/rm -f  ${datapath}/${analdate}/sfg_${analdate}_${charfhr}_ensmean
+         /bin/rm -f  ${datapath}/${analdate}/sfg_${analdate}_${charfhr}_enssprd
          echo "running ${execdir}/getsigensmeanp_smooth.x ${datapath2}/ sfg_${analdate}_${charfhr}_ensmean sfg_${analdate}_${charfhr} ${nanals} sfg_${analdate}_${charfhr}_enssprd"
          export PGM="${execdir}/getsigensmeanp_smooth.x ${datapath2}/ sfg_${analdate}_${charfhr}_ensmean sfg_${analdate}_${charfhr} ${nanals} sfg_${analdate}_${charfhr}_enssprd"
          ${enkfscripts}/runmpi
