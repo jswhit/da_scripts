b71794ce9dfa211f4b6f55358e2e723e338317ee
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2022-07-22 15:11:32 +0000

    update

--- compile_ufs_p8c_atm.sh
+++ compile_ufs_p8c_atm.sh
@@ -3,11 +3,7 @@ git clone https://github.com/ufs-community/ufs-weather-model ufs-weather-model-p
 cd ufs-weather-model-p8c-atm
 git checkout Prototype-P8c
 git submodule update --init --recursive
-
-# patch to turn off saving of IAU increments in MOM6 restart
-patch -p 0 < /scratch2/BMC/gsienkf/whitaker/mom6_iau_restart.diff.new
 # add no nsst version of p8 suite
 /bin/cp -f /scratch2/BMC/gsienkf/whitaker/suite_FV3_GFS_v17_p8_nonsst.xml FV3/ccpp/suites
-
 cd tests
 ./compile.sh hera.intel "-DAPP=ATM -D32BIT=ON -DCCPP_SUITES=FV3_GFS_v16,FV3_GFS_v16_no_nsst,FV3_GFS_v17_p8,FV3_GFS_v17_p8_nonsst" atm YES NO
