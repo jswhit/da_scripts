96f97fb6e0a1912cff0a3384aed33945de0c50b3
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2022-11-03 20:52:37 +0000

    update

--- submit_job.sh
+++ submit_job.sh
@@ -1,4 +1,8 @@
 # sh submit_job.sh <machine>
 machine=$1
-cat ${machine}_preamble config.sh > job.sh
+if [ -z $2 ]; then
+   cat ${machine}_preamble config.sh > job.sh
+else
+   cat ${machine}_preamble2 config.sh > job.sh
+fi
 sbatch job.sh
