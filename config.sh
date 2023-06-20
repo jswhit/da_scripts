61c6a259457a8dedabf742736c6fcb31f7bbf23c
Jeffrey S Whitaker <jwhitake@hercules-login-2.hpc.msstate.edu>
2023-06-20 13:47:47 -0500

    update

--- config.sh
+++ config.sh
@@ -58,7 +58,7 @@ export recenter_anal="true"
 export recenter_fcst="false"
 
 # override values from above for debugging.
-export cleanup_ensmean='false'
+#export cleanup_ensmean='false'
 #export recenter_fcst="false"
 #export cleanup_observer='false'
 #export cleanup_controlanl='false'
