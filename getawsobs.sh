fe041d7c4aa0eab3132e8834827a21d5b7009f57
Jeffrey S Whitaker <jwhitake@hercules-login-4.hpc.msstate.edu>
2023-11-10 09:56:25 -0600

    script to fetch obs from aws

---
+++ getawsobs.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+which aws
+if [ $? -ne 0 ]; then
+   module purge
+   # hera
+   #module use /scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core
+   #module use /scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.0/envs/unified-env/install/intel/2021.5.0
+   #module load stack-intel/2021.5.0
+   # hercules
+   module use /work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core
+   module use /work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/intel-oneapi-mpi/2021.9.0/intel/2021.9.0
+   module load stack-intel/2021.9.0
+
+   module load awscli
+fi
+YYYYMMDDHH=$1 # analysis date.
+obtyp=${2:-"all"} # specify single ob type, default is all obs.
+YYYYMM=`echo $YYYYMMDDHH | cut -c1-6`
+YYYYMMDD=`echo $YYYYMMDDHH | cut -c1-8`
+HH=`echo $YYYYMMDDHH | cut -c9-10`
+DD=`echo $YYYYMMDDHH | cut -c7-8`
+MM=`echo $YYYYMMDDHH | cut -c5-6`
+YYYY=`echo $YYYYMMDDHH | cut -c1-4`
+CDUMP='gdas'
+OUTPATH=/work/noaa/gsienkf/whitaker/awsobs
+S3PATH=/noaa-reanalyses-pds/observations/reanalysis
+# directory structure required by global-workflow
+TARGET_DIR=${OUTPATH}/${CDUMP}.${YYYYMMDD}/${HH}/atmos
+mkdir -p $TARGET_DIR
+obtypes=("airs" "amsua" "amsub" "amv" "atms" "cris" "cris" "geo" "geo" "gps" "hirs" "hirs" "hirs" "iasi" "mhs" "msu" "saphir" "seviri" "ssmi" "ssmis" "ssu")
+if [ $YYYYMMDDHH -lt "2009050106" ]; then
+# before 2009050106 for amsua use nasa/r21c_repro/gmao_r21c_repro
+   dirs=("nasa" "nasa/r21c_repro" "1bamub" "merged" "atms" "cris" "crisf4" "goesnd" "goesfv" "gpsro" "1bhrs2" "1bhrs3" "1bhrs4" "mtiasi" "1bmhs" "1bmsu" "saphir" "sevcsr""eumetsat" "eumetsat" "1bssu")
+   obnames=("aqua" "1bamu" "1bamub" "satwnd" "atms" "cris" "crisf4" "goesnd" "goesfv" "gpsro" "1bhrs2" "1bhrs3" "1bhrs4" "mtiasi" "1bmhs" "1bmsu" "saphir" "sevcsr" "ssmit" "ssmisu" "1bssu")
+   dumpnames=("airs_disc_final" "gmao_r21c_repro" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas")
+else
+   dirs=("nasa" "1bamua" "1bamub" "merged" "atms" "cris" "crisf4" "goesnd" "goesfv" "gpsro" "1bhrs2" "1bhrs3" "1bhrs4" "mtiasi" "1bmhs" "1bmsu" "saphir" "sevcsr" "eumetsat" "eumetsat" "1bssu")
+   obnames=("aqua" "1bamua" "1bamub" "satwnd" "atms" "cris" "crisf4" "goesnd" "goesfv" "gpsro" "1bhrs2" "1bhrs3" "1bhrs4" "mtiasi" "1bmhs" "1bmsu" "saphir" "sevcsr" "ssmit" "ssmisu" "1bssu")
+   dumpnames=("airs_disc_final" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas" "gdas")
+fi
+for n in ${!obtypes[@]}; do
+  if [ ${obtypes[$n]} == $obtyp ] || [ $obtyp == "all" ]; then
+     if [ ${obtypes[$n]} == "airs" ] && [ ${dirs[$n]} == "nasa" ]; then
+        # NASA airs obs
+        s3file=s3:/"${S3PATH}/${obtypes[$n]}/${dirs[$n]}/${obnames[$n]}/${YYYY}/${MM}/bufr/${dumpnames[$n]}.${YYYYMMDD}.t${HH}z.bufr"
+        localfile="${TARGET_DIR}/${CDUMP}.t${HH}z.airsev.tm00.bufr_d"
+     elif [ ${obtypes[$n]} == "amsua" ] && [ ${dirs[$n]} == "nasa/r21c_repro" ]; then
+        s3file=s3:/"${S3PATH}/${obtypes[$n]}/${dirs[$n]}/${YYYY}/${MM}/bufr/${dumpnames[$n]}.${YYYYMMDD}.t${HH}z.${obnames[$n]}.tm00.bufr"
+        localfile="${TARGET_DIR}/${CDUMP}.t${HH}z.1bamua.tm00.bufr_d"
+     else
+        s3file=s3:/"${S3PATH}/${obtypes[$n]}/${dirs[$n]}/${YYYY}/${MM}/bufr/${dumpnames[$n]}.${YYYYMMDD}.t${HH}z.${obnames[$n]}.tm00.bufr_d"
+        localfile="${TARGET_DIR}/${CDUMP}.t${HH}z.${obnames[$n]}.tm00.bufr_d"
+     fi
+     aws s3 ls --no-sign-request $s3file
+     aws s3 cp --no-sign-request $s3file $localfile
+     ls -l $localfile
+  fi
+done
+# prepbufr
+obtypes="prepbufr prepbufr.acft_profiles"
+for obtype in $obtypes; do
+   if [ ${obtypes[$n]} == $obtyp ] || [ $obtyp == "all" ]; then
+      if [ $obtype == "prepbufr" ]; then
+         s3file=s3:/"${S3PATH}/conv/${obtype}/${YYYY}/${MM}/prepbufr/gdas.${YYYYMMDD}.t${HH}z.${obtype}.nr"
+      else
+         s3file=s3:/"${S3PATH}/conv/${obtype}/${YYYY}/${MM}/bufr/gdas.${YYYYMMDD}.t${HH}z.${obtype}.nr"
+      fi
+      localfile="${TARGET_DIR}/${CDUMP}.t${HH}z.${obtype}"
+      aws s3 ls --no-sign-request $s3file
+      aws s3 cp --no-sign-request $s3file $localfile
+      ls -l $localfile
+   fi
+done
+# ozone
+# CFSR
+if [ $obtyp == "osbuv8" ] || [ $obtyp == "all" ]; then
+   s3file=s3:/"${S3PATH}/ozone/cfsr/${YYYY}/${MM}/bufr/gdas.${YYYYMMDD}.t${HH}z.osbuv8.tm00.bufr_d"
+   localfile="${TARGET_DIR}/${CDUMP}.t${HH}z.osbuv8.tm00.bufr_d"
+   aws s3 ls --no-sign-request $s3file
+   aws s3 cp --no-sign-request $s3file $localfile
+   ls -l $localfile
+fi
+# NASA bufr
+if [ $obtyp == "sbuv_v87" ] || [ $obtyp == "all" ]; then
+   s3file=s3:/"${S3PATH}/ozone/nasa/sbuv_v87/${YYYY}/${MM}/bufr/sbuv_v87.${YYYYMMDD}.${HH}z.bufr"
+   localfile="${TARGET_DIR}/${CDUMP}.t${HH}z.sbuv_v87.tm00.bufr_d"
+   aws s3 ls --no-sign-request $s3file
+   aws s3 cp --no-sign-request $s3file $localfile
+   ls -l $localfile
+fi
+# NASA netcdf
+obtypes=("ozone" "ozone" "ozone" "ozone" "ozone")
+dirs=("nasa" "nasa" "nasa" "nasa" "nasa")
+obnames=("mls" "omi-eff" "omps-lp" "omps-nm-eff" "omps-nm")
+dumpnames=("MLS-v5.0-oz" "OMIeff-adj" "OMPS-LPoz-Vis" "OMPSNM" "OMPSNP")
+for n in ${!obtypes[@]}; do
+  if [ ${obtypes[$n]} == $obtyp ] || [ $obtyp == "all" ]; then
+     s3file=s3:/"${S3PATH}/${obtypes[$n]}/${dirs[$n]}/${obnames[$n]}/${YYYY}/${MM}/netcdf/${dumpnames[$n]}.${YYYYMMDD}_${HH}z.nc"
+     localfile="${TARGET_DIR}/${dumpnames[$n]}.${YYYYMMDD}_${HH}z.nc"
+     aws s3 ls --no-sign-request $s3file
+     aws s3 cp --no-sign-request $s3file $localfile
+     ls -l $localfile
+  fi
+done
