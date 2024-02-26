71e21a7e37cb6017acdf830b55daed717d14cbb0
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2024-02-26 18:29:52 -0500

    update

--- s3archive.sh
+++ s3archive.sh
@@ -4,13 +4,9 @@ which aws
 if [ $? -ne 0 ]; then
    echo "SLURM_CLUSTER_NAME=$SLURM_CLUSTER_NAME"
    if  [ $SLURM_CLUSTER_NAME == 'es' ]; then #
-      #source /lustre/f2/dev/role.epic/contrib/Lmod_init.sh
-      #module use /lustre/f2/dev/wpo/role.epic/contrib/spack-stack/c4/modulefiles
-      #module use /lustre/f2/dev/wpo/role.epic/contrib/spack-stack/c4/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core
-      #module load stack-intel/2022.0.2
-      module use /ncrc/proj/epic/spack-stack/spack-stack-1.5.1/envs/unified-env/install/modulefiles/Core
+      module use /ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core
       module load stack-intel/2023.1.0
-      module load awscli
+      module load awscli-v2
    elif [ $SLURM_CLUSTER_NAME == 'hercules' ]; then
       module purge
       module use /work/noaa/epic/role-epic/spack-stack/hercules/modulefiles
